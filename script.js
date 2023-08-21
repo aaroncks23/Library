c7ba5eff9824813d50bf21814d1b05ee97423c5b
Aaron Chong <aarncks23@hotmail.com>
2023-08-22 02:11:47 +0800

    Add functionality to display book details on screen

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
     this.read = read
 }
 
-const book1 = new Book("title1","author1", "pages1", true);
-const book2 = new Book("title2","author2", "pages2", false);
+const book1 = new Book("title112301238dsdsdsd","author1", 12, true);
+const book2 = new Book("title2","author2", 23, false);
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
@@ -22,9 +22,35 @@ const fragment = new DocumentFragment();
 myLibrary.forEach(book => {
     const div = document.createElement('div');
     div.classList.add('book');
-    div.textContent = book.title;
+    
+    const title = document.createElement('div');
+    title.classList.add('title');
+    title.textContent = book.title;
+    div.appendChild(title);
+    
+    const author = document.createElement('div');
+    author.classList.add('author');
+    author.textContent = book.author;
+    div.appendChild(author);
+    
+    const pagesRead = document.createElement('div');
+    pagesRead.classList.add('pages-read');
+    
+    const pages = document.createElement('div');
+    // pages.classList.add('pages');
+    pages.textContent = "Pages: " + book.pages;
+    pagesRead.appendChild(pages);
+
+    const markRead = document.createElement('div');
+    markRead.classList.add('mark-read');
+    markRead.textContent = "Mark as Read ";
+    pagesRead.appendChild(markRead);
+
+    div.appendChild(pagesRead);
+
     fragment.appendChild(div);
-})
+
+});
 
 books.appendChild(fragment);
 
